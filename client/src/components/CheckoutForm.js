221db08fee916adf38d02e07d936eea0e462d790
ClydeFrog04 <egan.randal@gmail.com>
2020-06-12 17:25:18 -0600

    useForm working

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import {useForm} from "../hooks/useForm";
 
 const initialValue = {
   firstName: "",
@@ -15,16 +16,17 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  //const [values, setValues] = useState(initialValue);
+  const [values, clearForm, handleSubmit, handleChanges] = useForm("checkoutForm", initialValue, setShowSuccessMessage);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
+  // const handleChanges = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+  // };
+  //
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setShowSuccessMessage(true);
+  // };
 
   return (
     <>
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,26 @@
 // write your custom hook here to control your checkout form
+
+import {useLocalStorage} from "./useLocalStorage";
+
+export const useForm = (key, initialValues, cb) => {
+    const [values, setValues] = useLocalStorage(key, initialValues);
+
+    const handleChanges = e => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value//set the value of the given key
+        });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        cb(true);
+    };
+
+    const clearForm = e =>{
+        e.preventDefault();
+        setValues(initialValues);
+    }
+
+    return [values, clearForm, handleSubmit, handleChanges];
+}
